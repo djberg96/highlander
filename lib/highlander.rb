38fa7b340ecfac6c9d0482bea8e5faf78031c684
Daniel Berger <djberg96@gmail.com>
2010-12-12 07:14:10 -0700

    Make a special exception for irb.

--- lib/highlander.rb
+++ lib/highlander.rb
@@ -1,13 +1,17 @@
 module Highlander
+  # The version of the highlander library
   VERSION = '0.1.0'
-  BEGIN{
-    program = File.expand_path($0)
-    DATA = File.open(program) unless defined?(DATA)
-    status = DATA.flock(File::LOCK_EX | File::LOCK_NB)
 
-    if status != 0
-      raise RuntimeError, "Program '#{program}' already running"
-      exit! # In case of rescue, forcibly bail
+  BEGIN{
+    unless $0 == 'irb'
+      program = File.expand_path($0)
+      DATA = File.open(program) unless defined?(DATA)
+      status = DATA.flock(File::LOCK_EX | File::LOCK_NB)
+
+      if status != 0
+        raise RuntimeError, "Program '#{program}' already running"
+        exit! # In case of rescue, forcibly bail
+      end
     end
   }
 end

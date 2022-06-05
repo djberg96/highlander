276a20968d4f0b8f9e9acb5a77174d6c139c856f
Daniel Berger <djberg96@gmail.com>
2022-06-05 13:59:13 -0400

    Update specs to check for output.

--- spec/highlander_spec.rb
+++ spec/highlander_spec.rb
@@ -8,6 +8,7 @@ require 'highlander'
 
 RSpec.describe Highlander do
   let(:filename){ 'highlander_test.rb' }
+  let(:outfile){ 'highlander_output.txt' }
 
   before do
     File.open(filename, 'w') do |fh|
@@ -18,6 +19,7 @@ RSpec.describe Highlander do
 
   after do
     File.delete(filename) if File.exist?(filename)
+    File.delete(outfile) if File.exist?(outfile)
   end
 
   example "version" do
@@ -28,11 +30,13 @@ RSpec.describe Highlander do
   example "attempting to run multiple instances will fail" do
     pid1 = Process.spawn("ruby #{filename}", [:out, :err] => IO::NULL)
     sleep 1 # Make sure pid1 runs first, is there a better way?
-    pid2 = Process.spawn("ruby #{filename}", :err => IO::NULL, :out => IO::NULL)
+    pid2 = Process.spawn("ruby #{filename}", :err => [outfile, 'w'], :out => IO::NULL)
+
     status1 = Process.waitpid2(pid1).last
     status2 = Process.waitpid2(pid2).last
 
     expect(status1.exitstatus).to eq(0)
     expect(status2.exitstatus).not_to eq(0)
+    expect(File.read(outfile)).to include("There can be only one!")
   end
 end

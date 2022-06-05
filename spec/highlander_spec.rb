91fab891bb7aaca5c56bab048b6c66d27c832351
Daniel Berger <djberg96@gmail.com>
2022-06-05 12:48:58 -0400

    Minor spawn refactor.

--- spec/highlander_spec.rb
+++ spec/highlander_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe Highlander do
   end
 
   example "attempting to run multiple instances will fail" do
-    pid1 = Process.spawn("ruby #{filename}", :err => IO::NULL, :out => IO::NULL)
+    pid1 = Process.spawn("ruby #{filename}", [:out, :err] => IO::NULL)
     sleep 1 # Make sure pid1 runs first, is there a better way?
     pid2 = Process.spawn("ruby #{filename}", :err => IO::NULL, :out => IO::NULL)
     status1 = Process.waitpid2(pid1).last
